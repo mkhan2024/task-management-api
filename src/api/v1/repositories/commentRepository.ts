a9c095d06793e9ffc1c720ba1d89f896d02a80e1
Apurba <mkhan14@rrc.ca>
2026-04-12 23:48:07 -0500

    feat: add Firestore repositories for Project, Task and Comment

---
+++ src/api/v1/repositories/commentRepository.ts
@@ -0,0 +1,27 @@
+import { db } from "../../config/firebaseConfig";
+import { Comment } from "../models/commentModel";
+
+const COLLECTION = "comments";
+
+export const commentRepository = {
+    async create(comment: Omit<Comment, "id">): Promise<Comment> {
+        const docRef = await db.collection(COLLECTION).add({
+            ...comment,
+            createdAt: new Date(),
+        });
+        const doc = await docRef.get();
+        return { id: doc.id, ...doc.data() } as Comment;
+    },
+
+    async findByTaskId(taskId: string): Promise<Comment[]> {
+        const snapshot = await db.collection(COLLECTION)
+            .where("taskId", "==", taskId)
+            .get();
+        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Comment));
+    },
+
+    async delete(id: string): Promise<boolean> {
+        await db.collection(COLLECTION).doc(id).delete();
+        return true;
+    }
+};
